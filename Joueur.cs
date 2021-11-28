6faf439b0108f7c9c3e10bb608c2b2f79fc5b4ff
ntacv <ntackn1@gmail.com>
2021-11-28 14:42:30 +0100

    début joueur

---
+++ Joueur.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Scrabble
+{
+    class Joueur
+    {
+        //Attributs
+        string nom;
+        int score;
+        string[] mots;
+
+        //Constructeur
+        public Joueur(string nom)
+        {
+            this.nom = nom;
+            score = 0;
+            mots = null;
+        }
+        public Joueur()
+        {
+
+        }
+
+
+    }
+}
